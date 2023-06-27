7fa37e26095af8eb5ec5b5ef0c30229824f56a3b
Antonio69999 <antoine.chabeauti@gmail.com>
2023-06-27 14:36:17 +0200

    php_part1

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+//Exercice_1
+$name = "Bob";
+
+//Exercice_2
+$lastname = "Toto";
+$firstname = "Boby";
+$age = 22;
+
+echo $lastname;
+echo $firstname;
+echo $age;
+
+//Exercice_3
+$km = 1;
+echo $km;
+$km = 3;
+echo $km;
+$km = 125;
+echo $km;
+
+//Exercice_4
+
+$string = "string";
+$int = 10;
+$float = 10.5;
+$bool = true; // or false
+
+echo $string, $int, $float, $bool;
+
+//Exercice_5
+
+$int_var;
+echo $int_var=10;
+
+//Exercice_6
+$name6 = "Bob";
+echo "Bonjour $name6, comment vas tu ?";
+
+//Exercice_7
+echo "Bonjour $lastname $firstname, tu as $age ans";
+
+//Exercice_8
+
+$calc_addition = 3 + 4;
+$calc_mult = 5 * 20;
+$calc_div = 45 / 5;
+
+echo $calc_addition, $calc_mult, $calc_div;
+?>
